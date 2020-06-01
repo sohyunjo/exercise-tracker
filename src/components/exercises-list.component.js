1a572eeb3f687a452b087a5fbfa9ccc659cfd7b8
So Hyun Jo <sohyunjo9@gmail.com>
2020-05-31 22:38:25 -0400

    done

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -17,7 +17,6 @@ const Exercise = props => (
 export default class ExercisesList extends Component {
   constructor(props) {
     super(props);
-
     this.deleteExercise = this.deleteExercise.bind(this);
     this.state = {
       exercises: [],
